b8443805dc30a44e7bf9fa76956f2e6e855729b2
frankito1012 <frankitogrz@gmail.com>
2015-11-14 08:49:54 -0800

    semana 11

---
+++ semana 11/js/index.js
@@ -0,0 +1,20 @@
+/* a Pen by DIACO : twitter.com/Diaco_ml || codepen.io/MAW
+powered by GSAP : http://www.greensock.com/
+*/
+
+var words=['Rocky','Ferg','Nast','Twelvy','','Dom','Ant'],
+    centerT=document.getElementById('center-txt'),leftT=document.getElementById('left-txt');
+    W1=leftT.offsetWidth,W2=centerT.offsetWidth,tl=new TimelineMax({repeat:-1});
+
+TweenLite.set('#center-txt',{left:W1});
+TweenLite.set('#right-txt',{left:W1+W2});
+
+for(var i=0;i<words.length;i++){
+  centerT.innerHTML=words[i];
+  var W3=centerT.offsetWidth;
+  tl.to('#right-txt',1,{x:120,ease:Sine.easeOut},'+=3')
+    .to('#center-txt',1,{text:{value:words[i]},ease:Sine.easeOut},'-=0.7')
+    .to('#right-txt',1.7,{x:W3-W2,ease:Bounce.easeOut},'-=0.1')
+};
+
+// a Pen by DIACO : twitter.com/Diaco_ml || codepen.io/MAW 
